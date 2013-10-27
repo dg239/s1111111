73998cf9a20676abd678c9b8f82a85053bf94dbf
dg239 <mail@thijstervelde.com>
2013-10-27 15:09:12 -0700

    This file is version number 1

---
+++ blogheader.php
@@ -0,0 +1,16 @@
+<header>
+	<h1>My first blog</h1>
+	<nav>
+		<ul>
+			<li>
+				<a href="blog.php">Overview</a>
+			</li>
+			<li>
+				<a href="addpost.php">Add post</a>
+			</li>
+			<li>
+				<a href="addauthor.php">Add author</a>
+			</li>
+		</ul>qweqweWasdasdasdsdasdasd
+	</nav>qweqwe
+</header>
